38301c6c70ce958bd9122a303fe63e5c5aab90e7
daniboy01 <pappdaniel012@gmail.com>
2020-03-21 13:14:19 +0100

    remove funny commit hahaha

--- src/tiles/PositionedImage.java
+++ src/tiles/PositionedImage.java
@@ -14,7 +14,6 @@ public class PositionedImage {
         this.posX = posX;
         this.posY = posY;
         try {
-            //Cica
             image = ImageIO.read(new File(filename));
         } catch (IOException e) {
             e.printStackTrace();
